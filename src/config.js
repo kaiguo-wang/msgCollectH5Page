95045c6189be54b887e9f3cd13f26505add067db
18665981309@163.com <18665981309@163.com>
2020-06-30 18:12:56 +0800

    优化代码

--- src/config.js
+++ src/config.js
@@ -1,13 +1,4 @@
 export default {
-	// local_ip: 'http://localhost:3000',
 	ipH5: 'https://sfkc.sf-express.com',
-	ip: '/microservice',
-	// no_encry_ip: location.origin,
-	no_encry_ip: window.location.origin, // 微伙伴宠宝线上
-	// no_encry_ip: 'http://10.0.126.33:8080', // 微伙伴宠宝本地
-	// no_encry_ip: location.origin,
-	isDecode: true,
-	timeout: 10000,
-	imgHost: `${window.location.origin}/aircap/wxopen/viewPic.shtml?fileName=` // 线上
-	// imgHost: 'http://10.0.126.91:8081/scc-web/aircap/download/downloadImportTemplate.shtml?fileName=' // 测试
+	timeout: 10000
 };
--- src/pages/msgCollect/api/index/index.js
+++ src/pages/msgCollect/api/index/index.js
@@ -1,15 +1,15 @@
 import axios from '@/util/axios'
 
-// 发送验证验 // 后端黄伟雄
+// 发送验证验
 const sendCodeApi = (obj) => axios('post', '/subCustomerApp/sendVerificationCodeToUser', obj)
 
-// 提交 // 后端黄伟雄
+// 提交
 const submitApi = (obj) => axios('post', '/subCustomerApp/subCustomerRegister', obj)
 
-// 查询 // 后端黄伟雄
+// 查询
 const searchApi = (obj) => axios('post', '/subCustomerApp/subCustomerAddressInfo', obj)
 
-// 新增地址 // 后端黄伟雄
+// 新增地址
 const addAddressApi = (obj) => axios('post', '/subCustomerApp/subCustomerAddressInsert', obj)
 
 export { sendCodeApi, submitApi, searchApi, addAddressApi }
